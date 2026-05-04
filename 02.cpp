551d09a84e3dc116a8d84a494e7389cce2b1d5af
ajitrai1507 <ajitrai1507@gmail.com>
2026-05-04 23:22:10 +0530

    add .gitignore and initial implementation of a C++ program to calculate the sum of two numbers

---
+++ 02.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b, sum;
+
+    cout<<"Enter the first numner"<<endl;
+    cin>>a; 
+    cout<<"Enter the first numner"<<endl;
+    cin>>b;
+
+    sum = a+b;
+    cout<<"Sum of two number is "<<sum<<endl;
+}
