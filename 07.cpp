5ec2d320fed2eede4a030a02be5ce25f650f5708
ajitrai1507 <ajitrai1507@gmail.com>
2026-05-06 23:39:22 +0530

    add initial implementations of C++ programs for sum calculation and package acceptance

---
+++ 07.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b, sum;
+    cout<<"Enter the first number"<<endl;
+    cin>>a;
+    cout<<"Enter the second number"<<endl;
+    cin>>b;
+
+    sum = a+b;
+    cout<<"Sum of the two number is "<<sum;
+    
+}
---
+++ 08.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int package;
+    cout<<"Enter the package"<<endl;
+    cin>>package;
+
+    if(package>10){
+        cout<<"I accept the offer"<<endl;
+    }else{
+        cout<<"I will not accept the offer";
+    }
+}
